391520c942f4331196b3e32047b6298ac7905945
Adnan <adnan@klynetech.com>
2025-10-03 10:29:20 +0530

    verification bagde fix

--- app/page.js
+++ app/page.js
@@ -92,13 +92,17 @@ export default function Home() {
             {/* Verification Badge - prominent and centered */}
             <div className="flex flex-col items-center mb-6">
               {/* <div className="bg-gradient-to-tr from-blue-400 to-indigo-500 rounded-full p-2 shadow-lg mb-2"> */}
-                <Image
+                {/* <Image
                   src="https://static.vecteezy.com/system/resources/thumbnails/047/309/930/small/verified-badge-profile-icon-png.png"
                   alt="Verification Badge"
                   width={72}
                   height={72}
                   priority
-                />
+                /> */}
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="72" height="72" viewBox="0 0 48 48">
+                  <polygon fill="#42a5f5" points="29.62,3 33.053,8.308 39.367,8.624 39.686,14.937 44.997,18.367 42.116,23.995 45,29.62 39.692,33.053 39.376,39.367 33.063,39.686 29.633,44.997 24.005,42.116 18.38,45 14.947,39.692 8.633,39.376 8.314,33.063 3.003,29.633 5.884,24.005 3,18.38 8.308,14.947 8.624,8.633 14.937,8.314 18.367,3.003 23.995,5.884"></polygon>
+                  <polygon fill="#fff" points="21.396,31.255 14.899,24.76 17.021,22.639 21.428,27.046 30.996,17.772 33.084,19.926"></polygon>
+                </svg>
               {/* </div> */}
               <h1 className="text-2xl font-bold text-gray-900 dark:text-white text-center mt-2">
                 Instagram Verification Badge
